4ac13d8b63b2d3c1bd3b1cdd351fe31bd81db792
BellacusDev <bellacus.dev@gmail.com>
2023-10-23 19:59:11 +0200

    fixed img public

--- src/pages/work/myWork.jsx
+++ src/pages/work/myWork.jsx
@@ -7,7 +7,13 @@ import Contact from '../../components/container/contact';
 /* Styles */
 import { PageStiled, SectionStyles } from '../../styles/CommonPages'
 
+import ImageItem from '../../components/pure/ImageItem';
 
+
+
+const Galery = styled.div`
+  
+`;
 
 const MyWork = () => {
 
@@ -18,10 +24,19 @@ const MyWork = () => {
     { id: 4, url: '/contact', text: 'Contacto' }  
   ]
 
+  const imageItems = [
+    { id: 1, url: '/public/portfolio.png', title: 'Portafolio', img: 'portfolio.png' },  
+  ]
+
   return (
     <PageStiled>
       <Header links={headerLinks}></Header>
-      <SectionStyles id='work'></SectionStyles>
+      <SectionStyles id='work'>
+        <Galery>
+          <ImageItem links={imageItems}></ImageItem>  
+        </Galery>
+      </SectionStyles>
+      
       <Contact></Contact>
       <Footer></Footer>
     </PageStiled>
